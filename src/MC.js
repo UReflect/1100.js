f61188ebd080fe33a2b9c3b77b00349f5798f9e8
Adrien <adrien.milcent@epitech.eu>
2017-10-17 19:27:18 -0700

    Add drag and drop

--- src/MC.js
+++ src/MC.js
@@ -24,12 +24,61 @@ class MC {
 
     if (Object.keys(options).includes('container')) this._container = options.container;
 
-    // if (!Object.keys(options).includes('draggable') || (Object.keys(options).includes('draggable') && options.draggable == true)) {
-    //   $(this._container).sortable({
-    //     grid: [ $(window).width() / 10, $(window).height() / 10 ]
-    //   });
-    // }
+    if (!Object.keys(options).includes('draggable') || (Object.keys(options).includes('draggable') && options.draggable == true)) {
+      var elements = document.querySelectorAll(this._identifier);
+      this.setupDraggable(elements);
+    }
     if (!Object.keys(options).includes('resizable') || (Object.keys(options).includes('resizable') && options.resizable == true)) this._resizable = new Resizable(this._identifier);
+  }
+
+  setupDraggable(elements) {
+    self = this;
+    Array.prototype.forEach.call(elements, function(el, i) {
+      var isDragReady = false;
+      var dragoffset = {
+        elem: null,
+        x: 0,
+        y: 0
+      };
+      // el.setAttribute('draggable', true);
+      el.style.position = "absolute";
+
+      el.addEventListener('mousedown', function (e) {
+        e.preventDefault();
+        isDragReady = true;
+
+        if (!e.pageX) {
+          e.pageX = e.clientX + (document.documentElement.scrollLeft ?
+            document.documentElement.scrollLeft :
+            document.body.scrollLeft);
+
+          e.pageY = e.clientY + (document.documentElement.scrollTop ?
+            document.documentElement.scrollTop :
+            document.body.scrollTop);
+        }
+        dragoffset.x = e.pageX - el.offsetLeft;
+        dragoffset.y = e.pageY - el.offsetTop;
+        dragoffset.elem = this;
+      });
+      el.addEventListener('mouseup', function () {
+        isDragReady = false;
+      });
+      document.addEventListener('mousemove', function (e) {
+        if (isDragReady) {
+          if (!e.pageX) {
+            e.pageX = e.pageX || e.clientX + (document.documentElement.scrollLeft ?
+              document.documentElement.scrollLeft :
+              document.body.scrollLeft);
+
+            e.pageY = e.pageY || e.clientY + (document.documentElement.scrollTop ?
+              document.documentElement.scrollTop :
+              document.body.scrollTop);
+          }
+          dragoffset.elem.style.top = (e.pageY - dragoffset.y) + "px";
+          dragoffset.elem.style.left = (e.pageX - dragoffset.x) + "px";
+        }
+      });
+    });
   }
 
   // Set listener on element
